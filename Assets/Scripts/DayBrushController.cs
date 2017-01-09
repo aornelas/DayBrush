03acf7fb72c91f2f4f9bfb12b21a6f336720536e
Andres Ornelas <andres@twitter.com>
2017-01-09 00:43:32 -0800

    Add undo with app button

--- Assets/Scripts/DayBrushController.cs
+++ Assets/Scripts/DayBrushController.cs
@@ -1,23 +1,36 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class DayBrushController : MonoBehaviour {
 
 	public GameObject pencil;
 	public GameObject paint;
-	private GameObject stroke;
+
+	private Stack<GameObject> strokes;
+
+	void Awake ()
+	{
+		strokes = new Stack<GameObject>();
+	}
 	
 	void Update ()
 	{
 		if (GvrController.ClickButtonDown) {
-			stroke = GameObject.Instantiate<GameObject>(paint);
+			GameObject stroke = GameObject.Instantiate<GameObject>(paint);
 			stroke.transform.SetParent(pencil.transform);
 			stroke.transform.localPosition = Vector3.zero + new Vector3(0, 0, -0.5f);
 			stroke.gameObject.GetComponent<TrailRenderer>().enabled = true;
+			strokes.Push(stroke);
 		}
 
 		if (GvrController.ClickButtonUp) {
+			GameObject stroke = strokes.Peek();
 			stroke.transform.SetParent(this.transform);
+		}
+
+		if (GvrController.AppButtonDown) {
+			GameObject stroke = strokes.Pop();
+			stroke.SetActive(false);
 		}
 
 	}
