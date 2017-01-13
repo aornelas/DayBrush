1c08e2a125b44ad42c58546bbd763f4b52f6f93c
Andres Ornelas <andres@twitter.com>
2017-01-12 17:44:36 -0800

    WIP: add save/load for a non-serializable object
    Color is not serializable, so we need to create a serialization
    surrogate and configure it in the binary formatter. This same pattern
    will be needed for any non-serializable objects that need to be
    saved/loaded.

--- Assets/Scripts/Painting.cs
+++ Assets/Scripts/Painting.cs
@@ -8,5 +8,5 @@ using System.Collections;
 public class Painting {
 
     public string name;
-    public int colorIndex;
+    public Color color;
 }
---
+++ Assets/Scripts/io/ColorSerializationSurrogate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Runtime.Serialization;
+
+sealed class ColorSerializationSurrogate : ISerializationSurrogate {
+
+    public void GetObjectData(System.Object obj, SerializationInfo info, StreamingContext context)
+    {
+        Color c = (Color) obj;
+        info.AddValue("r", c.r);
+        info.AddValue("g", c.g);
+        info.AddValue("b", c.b);
+        info.AddValue("a", c.a);
+    }
+
+    public System.Object SetObjectData(System.Object obj, SerializationInfo info, StreamingContext context,
+            ISurrogateSelector selector)
+    {
+        Color c = (Color) obj;
+        c.r = (float) info.GetValue("r", typeof(float));
+        c.g = (float) info.GetValue("g", typeof(float));
+        c.b = (float) info.GetValue("b", typeof(float));
+        c.a = (float) info.GetValue("a", typeof(float));
+        obj = c;
+        return c;
+    }
+}
