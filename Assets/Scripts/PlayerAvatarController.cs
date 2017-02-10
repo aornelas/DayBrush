457beb5bfb879863c7d17c166507c830305dd2d6
Andres Ornelas <andres@twitter.com>
2017-02-09 22:51:38 -0800

    hide player avatar locally

--- Assets/Scripts/PlayerAvatarController.cs
+++ Assets/Scripts/PlayerAvatarController.cs
@@ -18,4 +18,16 @@ public class PlayerAvatarController : NetworkBehaviour {
             head.transform.rotation = GameObject.FindGameObjectWithTag("MainCamera").transform.rotation;
         }
     }
+
+    public override void OnStartLocalPlayer()
+    {
+        GameObject head = transform.Find("head").gameObject;
+        GameObject visor = head.transform.Find("visor").gameObject;
+        GameObject body = transform.Find("body").gameObject;
+
+        // hide player avatar locally
+        head.GetComponent<MeshRenderer>().enabled = false;
+        visor.GetComponent<MeshRenderer>().enabled = false;
+        body.GetComponent<MeshRenderer>().enabled = false;
+    }
 }
