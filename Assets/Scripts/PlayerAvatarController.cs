8bb09e2832644f0d9fce2479a0643af6e1a51566
Andres Ornelas <andres@twitter.com>
2017-02-10 00:01:11 -0800

    make pencil network-aware

--- Assets/Scripts/PlayerAvatarController.cs
+++ Assets/Scripts/PlayerAvatarController.cs
@@ -5,31 +5,41 @@ using System.Collections;
 public class PlayerAvatarController : NetworkBehaviour {
 
     private GameObject head;
+    private GameObject pencil;
+    private GameObject camera;
+    private GameObject actualPencil;
 
     void Start ()
     {
         if (isLocalPlayer) {
             transform.SetParent(GameObject.FindGameObjectWithTag("Player").transform);
         }
-        head = transform.Find("head").gameObject;
+
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
+        actualPencil = GameObject.FindGameObjectWithTag("Pencil");
+        pencil.GetComponent<MeshRenderer>().material = actualPencil.GetComponent<MeshRenderer>().material;
     }
 
     void Update ()
     {
         if (isLocalPlayer) {
-            head.transform.rotation = GameObject.FindGameObjectWithTag("MainCamera").transform.rotation;
+            head.transform.rotation = camera.transform.rotation;
+            pencil.transform.rotation = actualPencil.transform.rotation;
+            pencil.transform.position = actualPencil.transform.position;
         }
     }
 
     public override void OnStartLocalPlayer()
     {
-        GameObject head = transform.Find("head").gameObject;
+        head = transform.Find("head").gameObject;
+        pencil = transform.Find("pencil").gameObject;
         GameObject visor = head.transform.Find("visor").gameObject;
         GameObject body = transform.Find("body").gameObject;
 
-        // hide player avatar locally
+        // hide player avatar and duplicate pencil locally
         head.GetComponent<MeshRenderer>().enabled = false;
         visor.GetComponent<MeshRenderer>().enabled = false;
         body.GetComponent<MeshRenderer>().enabled = false;
+        pencil.GetComponent<MeshRenderer>().enabled = false;
     }
 }
